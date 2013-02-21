b12f4a0b8d600d9ba305d1dc14a48c81a0f6e8e4
bvoisin <bvoisin@USER-PC>
2013-02-21 20:19:41 +0100

    With same like/hates

--- codestory_final/code-story-final-core/src/main/java/net/codestory2/Main.java
+++ codestory_final/code-story-final-core/src/main/java/net/codestory2/Main.java
@@ -1,8 +1,11 @@
 package net.codestory2;
 
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonParser;
@@ -12,16 +15,18 @@ import org.codehaus.jackson.type.TypeReference;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
 
 public class Main
 {
     private Multimap<String, Geek> likes = ArrayListMultimap.create();
-    
+
     private Multimap<String, Geek> hates = ArrayListMultimap.create();
-    
+
     private Map<String, Geek> geeksByEmail = Maps.newHashMap();
-    
-    public void parse(InputStream res) throws Exception{
+
+    public void parse(InputStream res) throws Exception
+    {
         JsonParser jsonParser = null;
 
         jsonParser = new JsonFactory().createJsonParser(res);
@@ -39,17 +44,17 @@ public class Main
             hates.put(geek.getHate2(), geek);
         }
     }
-    
-    public Geek getGeek(String name) {
+
+    public Geek getGeek(String name)
+    {
         return geeksByEmail.get(name);
     }
-    
-    
+
     public Main() throws Exception
     {
         parse(Main.class.getResource("/codestory2013.json").openStream());
     }
-    
+
     public static void main(String[] args) throws Exception
     {
         Main instance = new Main();
@@ -62,5 +67,27 @@ public class Main
         {
             System.out.println(geek);
         }
+    }
+
+    public Set<Geek> getGeeksThatLikeTheSameThings(Geek geek)
+    {
+        Set<Geek> geeks = Sets.newHashSet();
+        geeks.addAll(getIfNotNull(likes, geek.getLike1()));
+        geeks.addAll(getIfNotNull(likes, geek.getLike2()));
+        geeks.addAll(getIfNotNull(likes, geek.getLike3()));
+        return geeks;
+    }
+
+    public Set<Geek> getGeeksThatHateTheSameThings(Geek geek)
+    {
+        Set<Geek> geeks = Sets.newHashSet();
+        geeks.addAll(getIfNotNull(hates, geek.getHate1()));
+        geeks.addAll(getIfNotNull(hates, geek.getHate2()));
+        return geeks;
+    }
+
+    private Collection<Geek> getIfNotNull(Multimap<String, Geek> maps, String like)
+    {
+        return (like != null) ? maps.get(like) : (List<Geek>) Collections.<Geek> emptyList();
     }
 }
--- codestory_final/code-story-final-core/src/test/java/net/codestory2/TestMain.java
+++ codestory_final/code-story-final-core/src/test/java/net/codestory2/TestMain.java
@@ -1,6 +1,8 @@
 package net.codestory2;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,9 +20,35 @@ public class TestMain
     @Test
     public void testGetGeek() throws Exception
     {
-        Main main = new Main();
-        Geek bvo = main.getGeek("bvo123@123mail.org");
+        Geek bvo = instance.getGeek("bvo123@123mail.org");
 
         assertEquals("Benoît", bvo.getPrenom());
     }
+    
+    @Test
+    public void testSameLikesAsMe() throws Exception
+    {
+        Geek bvo = instance.getGeek("bvo123@123mail.org");
+        
+        Set<Geek> geeks=instance.getGeeksThatLikeTheSameThings(bvo);
+        System.out.println(geeks.size());
+    }
+    
+    @Test
+    public void testSameHatesAsMe() throws Exception
+    {
+        Geek bvo = instance.getGeek("bvo123@123mail.org");
+        
+        Set<Geek> geeks=instance.getGeeksThatHateTheSameThings(bvo);
+        System.out.println(geeks.size());
+    }
+    
+//    @Test
+//    public void testOrderedFriends() throws Exception
+//    {
+//        Geek bvo = instance.getGeek("bvo123@123mail.org");
+//        
+//        Set<Geek> geeks=instance.getOrderedGeeks(bvo);
+//        System.out.println(geeks.size());
+//    }
 }
